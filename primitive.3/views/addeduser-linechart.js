1af710f685296511911c106a0ad44a59847d93c4
Chany <lyhy0310@gmail.com>
2017-06-28 00:29:36 +0900

    #04 테이블 그리기 + issue 테이블이 짤림 + linechart도 같이 그려짐 ;ㅁ;

--- primitive.3/views/addeduser-linechart.js
+++ primitive.3/views/addeduser-linechart.js
@@ -65,6 +65,7 @@ console.log(bus);
           options.series[i].data.push({name:item.key_as_string,y:item.doc_count})
         })
         var chart = new Highcharts.Chart(options);
+        window.chart = new Highcharts.chart(options);
     });
     }
 
@@ -83,8 +84,199 @@ drawLineChart('/data/user_signedup.json', 0, "NewUser");
 drawLineChart('/data/user_deleted.json', 1, "LeaveUser");
 drawLineChart('/data/user_signedup.json', 2, "TotalAddedUser")
 
-// 빼는 거 프로미스를 해야 될 텐데 ...
-//
+// 빼는 거 프로미스를 해야 될 텐데 .//
+
+
+Highcharts.drawTable = function() {
+
+console.log("이거 실행 됬나?.........");
+   // user options
+   var tableTop = 300,
+       colWidth = 100,
+       tableLeft = 20,
+       rowHeight = 20,
+       cellPadding = 2.5,
+       valueDecimals = 1,
+       valueSuffix = ' 명';
+
+   // internal variables
+   var chart = this,
+       series = chart.series;
+       renderer = chart.renderer,
+       cellLeft = tableLeft;
+
+
+
+
+   // draw category labels
+   $.each(series[0].data, function(i, item) {
+       renderer.text(
+           item.name,
+           cellLeft + cellPadding,
+           tableTop + (i + 2) * rowHeight - cellPadding
+       )
+       .css({
+           fontWeight: 'bold'
+       })
+       .add();
+   });
+
+   $.each(series, function(i, serie) {
+       cellLeft += colWidth;
+
+       // Apply the cell text
+       renderer.text(
+               serie.name,
+               cellLeft - cellPadding + colWidth,
+               tableTop + rowHeight - cellPadding
+           )
+           .attr({
+               align: 'right'
+           })
+           .css({
+               fontWeight: 'bold'
+           })
+           .add();
+
+       $.each(serie.data, function(row, point) {
+           // Apply the cell text
+           renderer.text(
+                   Highcharts.numberFormat(point.y, valueDecimals) + valueSuffix,
+                   cellLeft + colWidth - cellPadding,
+                   tableTop + (row + 2) * rowHeight - cellPadding
+               )
+               .attr({
+                   align: 'right'
+               })
+               .add();
+
+           // horizontal lines
+           if (row == 0) {
+               Highcharts.tableLine( // top
+                   renderer,
+                   tableLeft,
+                   tableTop + cellPadding,
+                   cellLeft + colWidth,
+                   tableTop + cellPadding
+               );
+               Highcharts.tableLine( // bottom
+                   renderer,
+                   tableLeft,
+                   tableTop + (serie.data.length + 1) * rowHeight + cellPadding,
+                   cellLeft + colWidth,
+                   tableTop + (serie.data.length + 1) * rowHeight + cellPadding
+               );
+           }
+           // horizontal line
+           Highcharts.tableLine(
+               renderer,
+               tableLeft,
+               tableTop + row * rowHeight + rowHeight + cellPadding,
+               cellLeft + colWidth,
+               tableTop + row * rowHeight + rowHeight + cellPadding
+           );
+
+       });
+
+       // vertical lines
+       if (i == 0) { // left table border
+           Highcharts.tableLine(
+               renderer,
+               tableLeft,
+               tableTop + cellPadding,
+               tableLeft,
+               tableTop + (serie.data.length + 1) * rowHeight + cellPadding
+           );
+       }
+
+       Highcharts.tableLine(
+           renderer,
+           cellLeft,
+           tableTop + cellPadding,
+           cellLeft,
+           tableTop + (serie.data.length + 1) * rowHeight + cellPadding
+       );
+
+       if (i == series.length - 1) { // right table border
+
+           Highcharts.tableLine(
+               renderer,
+               cellLeft + colWidth,
+               tableTop + cellPadding,
+               cellLeft + colWidth,
+               tableTop + (serie.data.length + 1) * rowHeight + cellPadding
+           );
+       }
+
+   });
+
+
+};
+
+/**
+* Draw a single line in the table
+*/
+Highcharts.tableLine = function (renderer, x1, y1, x2, y2) {
+   renderer.path(['M', x1, y1, 'L', x2, y2])
+       .attr({
+           'stroke': 'silver',
+           'stroke-width': 1
+       })
+       .add();
+}
+
+
+
+var table_options = {
+
+  chart: {
+      renderTo: 'datatable',
+      events: {
+          load: Highcharts.drawTable
+      },
+      type: 'column',
+      borderWidth: 1
+  },
+
+  title: {
+      text: 'Daily New User'
+  },
+
+  subtitle: {
+      text: ' 2017-05-09 ~2017-06-08'
+  },
+  yAxis: {
+        title: {
+            text: 'Number of new Users'
+        }
+    },
+    xAxis: {
+     type: 'category'
+   },
+  legend: {
+      y: -300
+  },
+
+  series: [{},{},{}]
+};
+
+function drawTableChart(jsonfilePath, i, seriesName){
+    $.getJSON(jsonfilePath, function(data) {
+
+    table_options.series[i].name = seriesName
+    table_options.series[i].data =[];
+    data.forEach(function(item){
+      table_options.series[i].data.push({name:item.key_as_string,y:item.doc_count})
+    })
+    var chart = new Highcharts.Chart(table_options);
+});
+}
+
+
+
+drawTableChart('/data/user_signedup.json', 0, "NewUser");
+drawTableChart('/data/user_deleted.json', 1, "LeaveUser");
+drawTableChart('/data/user_signedup.json', 2, "TotalAddedUser")
 
 
 
